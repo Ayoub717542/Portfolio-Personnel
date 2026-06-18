992f3fdc32ac927ed5edd6bdb5cf501947194a7d
Ayoub Hadi <AyoubHadi856@gmail.com>
2026-06-18 15:42:46 +0100

    update send form

--- My_Protfolio/script.js
+++ My_Protfolio/script.js
@@ -44,3 +44,16 @@ if (localStorage.getItem("theme") === "dark") {
     icon.classList.add("fa-sun");
 }
 
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+
+document.querySelectorAll('section, article').forEach(el => {
+  observer.observe(el);
+});
+
+
